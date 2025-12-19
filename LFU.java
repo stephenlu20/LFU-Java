97de1cb0e3e9ba273a6a20299f806c508bdc66ac
Stephen <stephen.lu20@gmail.com>
2025-12-19 18:35:40 -0500

    In progress work on access method

--- LFU.java
+++ LFU.java
@@ -4,13 +4,52 @@ class FreqNode {
     FreqNode next;
     FreqNode prev;
     int value;
-    Set<LFUItem> items;
+    Set<Integer> items;
 
     public FreqNode() {
         next = this;
         prev = this;
         value = 0;
         items = new LinkedHashSet<>();
+    }
+}
+
+class LFUItem {
+    Object data;
+    FreqNode parent;
+
+    public LFUItem(Object data, FreqNode parent) {
+        this.data = data;
+        this.parent = parent;
+    }
+}
+
+class LFUCache {
+    HashMap<Integer, LFUItem> byKey;
+    FreqNode head;
+
+    public LFUCache() {
+        byKey = new HashMap<>();
+        head = new FreqNode();
+    }
+}
+
+public class LFU {
+    LFUCache lfuCache = new LFUCache();
+
+    public FreqNode access(Integer key) {
+        LFUItem tmp = lfuCache.byKey.get(key);
+        if (tmp == null) {
+            throw new NullPointerException("Key does not exist");
+        }
+
+        FreqNode freq = tmp.parent;
+        FreqNode next_freq = freq.next;
+
+        if (next_freq == lfuCache.head || next_freq.value != freq.value + 1) {
+            next_freq = getNewNode(freq.value + 1, freq, freq.next);
+        }
+        next_freq.items.add(key);
     }
 
     public void deleteNode(FreqNode node) {
@@ -28,28 +67,4 @@ class FreqNode {
 
         return newNode;
     }
-}
-
-class LFUItem {
-    Object data;
-    FreqNode parent;
-
-    public LFUItem(Object data, FreqNode parent) {
-        this.data = data;
-        this.parent = parent;
-    }
-}
-
-class LFUCache {
-    HashMap<FreqNode, LFUItem> byKey;
-    FreqNode head;
-
-    public LFUCache() {
-        byKey = new HashMap<>();
-        head = new FreqNode();
-    }
-}
-
-public class LFU {
-    LFUCache lfuCache = new LFUCache();
 }
